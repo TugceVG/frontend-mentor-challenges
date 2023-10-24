e9af105936304236b606a263e433d330f282306e
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-10-24 21:57:12 +0200

    chore(news-homepage): add hamburger menu

---
+++ news-homepage/script.js
@@ -0,0 +1,9 @@
+const toggleButton = document.getElementById('toggle-button');
+const navbar = document.getElementById('navbar');
+
+toggleButton.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
+
+
