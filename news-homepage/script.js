8709e234a11aa5f7e0c7808002bcd406eb568345
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-10-26 23:43:09 +0200

    chore(news-homepage): improve menu and responsive styles

--- news-homepage/script.js
+++ news-homepage/script.js
@@ -1,9 +1,23 @@
+// const source = require("./assets/images/image-web-3-mobile.jpg");
+
 const toggleButton = document.getElementById('toggle-button');
 const navbar = document.getElementById('navbar');
+const container = document.getElementById('container');
+const image = document.getElementById('image');
 
 toggleButton.addEventListener('click', () => {
     navbar.classList.toggle('active');
+    toggleButton.classList.toggle('opened');
+    container.classList.toggle("dark");
 });
+
+window.addEventListener('resize', function (event) {
+    const w = window.innerWidth;
+    if (w <= 768) {
+        image.src = "./assets/images/image-web-3-mobile.jpg";
+    }
+});
+
 
 
 
