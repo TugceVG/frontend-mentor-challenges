d475b5f77a77b8758f0c0229d2de8b22f716b637
Tugce Vatansever GUNDUZ <tugcevatansever91@gmail.com>
2023-12-05 22:00:27 +0100

    chore(coming-soon-page): update validation and style

--- coming-soon-page/script.js
+++ coming-soon-page/script.js
@@ -1,11 +1,22 @@
 function signUp() {
   const email = document.querySelector("#email").value;
   const errorMsg = document.querySelector("#error-msg");
+  const errorIcon = document.querySelector("#error-icon");
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   if (!emailRegex.test(email)) {
-    errorMsg.style.display = "block";
+    showEror(errorMsg, errorIcon);
   } else {
-    errorMsg.style.display = "none";
+    hideEror(errorMsg, errorIcon);
   }
+}
+
+function showEror(msg, icon) {
+  msg.style.display = "block";
+  icon.style.display = "block";
+}
+
+function hideEror(msg, icon) {
+  msg.style.display = "none";
+  icon.style.display = "none";
 }
